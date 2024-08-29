03708852b272fff95703f3db41581004732f9836
Arthur K <arthurk031@outlook.com>
2024-08-29 16:24:54 -0300

    Início

---
+++ Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprovadoReprovado
+{
+    public class Aluno
+    {
+        public string nome;
+        public int n1;
+        public int n2;
+        public int n3;
+        public double CalculoMedia(int n1, int n2, int n3)
+        {
+            double mediaAluno = n1 + n2 + n3;
+            return mediaAluno;
+        }
+        public Aluno(string nome, int nota1, int nota2, int nota3)
+        {
+            this.nome = nome;
+            n1 = nota1;
+            n2 = nota2;
+            n3 = nota3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AprovadoReprovado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao teste de boletim!\n \nInsira o nome do aluno:");
+            string nome = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Nome do aluno: {nome}\n\nNota máxima do 1º trimestre é 30;\nNota máxima do 2º e 3º trimestre é 35.\n\nInsira, linha por linha, as notas do aluno dos três trimestres:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            Aluno a = new Aluno(nome, n1, n2, n3);
+            Console.Clear();
+            if (a.n1 <= 30 && a.n2 <= 35 && a.n3 <= 35 && a.n1 >= 0 && a.n2 >= 0 && a.n3 >= 0)
+            {
+                double mediaAluno = a.CalculoMedia(a.n1, a.n2, a.n3);
+                if (mediaAluno > 60)
+                {
+                    Console.WriteLine($"Aluno - {a.nome};\n\nSituação - APROVADO!");
+                }
+                else
+                {
+                    double restante = 60 - mediaAluno;
+                    Console.WriteLine($"Aluno - {a.nome};\n\nSituação - REPROVADO!\nNota em débito - {restante}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valores incorretos.");
+            }
+        }
+    }
+}
