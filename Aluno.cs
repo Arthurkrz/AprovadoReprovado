fbe4300b54b6a1d297869787aa910f0c0a319caf
Arthur K <arthurk031@outlook.com>
2024-09-03 21:17:02 -0300

    Correções

--- Aluno.cs
+++ Aluno.cs
@@ -10,17 +10,16 @@ namespace AprovadoReprovado
         public int n1;
         public int n2;
         public int n3;
-        public double CalculoMedia(int n1, int n2, int n3)
-        {
-            double mediaAluno = n1 + n2 + n3;
-            return mediaAluno;
-        }
         public Aluno(string nome, int nota1, int nota2, int nota3)
         {
             this.nome = nome;
             n1 = nota1;
             n2 = nota2;
             n3 = nota3;
+        }
+        public double CalculoMedia(int n1, int n2, int n3)
+        {
+            return ((30 * n1) + (35 * n2) + (35 * n3)) / 100;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace AprovadoReprovado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bem vindo ao teste de boletim!\n \nInsira o nome do aluno:");
+            Console.WriteLine("Bem vindo ao teste de boletim!\n\nInsira o nome do aluno:");
             string nome = Console.ReadLine();
             Console.Clear();
-            Console.WriteLine($"Nome do aluno: {nome}\n\nNota máxima do 1º trimestre é 30;\nNota máxima do 2º e 3º trimestre é 35.\n\nInsira, linha por linha, as notas do aluno dos três trimestres:");
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
-            Aluno a = new Aluno(nome, n1, n2, n3);
+            Console.WriteLine($"Nome do aluno: {nome}\n\nInsira, linha por linha, as notas do aluno dos três trimestres:");
+            int nota1 = int.Parse(Console.ReadLine());
+            int nota2 = int.Parse(Console.ReadLine());
+            int nota3 = int.Parse(Console.ReadLine());
+            Aluno a = new Aluno(nome, nota1, nota2, nota3);
             Console.Clear();
-            if (a.n1 <= 30 && a.n2 <= 35 && a.n3 <= 35 && a.n1 >= 0 && a.n2 >= 0 && a.n3 >= 0)
+            if (a.n1 <= 100 && a.n2 <= 100 && a.n3 <= 100 && a.n1 >= 0 && a.n2 >= 0 && a.n3 >= 0)
             {
                 double mediaAluno = a.CalculoMedia(a.n1, a.n2, a.n3);
                 if (mediaAluno > 60)
@@ -24,8 +24,7 @@ namespace AprovadoReprovado
                 }
                 else
                 {
-                    double restante = 60 - mediaAluno;
-                    Console.WriteLine($"Aluno - {a.nome};\n\nSituação - REPROVADO!\nNota em débito - {restante}");
+                    Console.WriteLine($"Aluno - {a.nome};\n\nSituação - REPROVADO!\nNota em débito - {60 - mediaAluno:F2}");
                 }
             }
             else
